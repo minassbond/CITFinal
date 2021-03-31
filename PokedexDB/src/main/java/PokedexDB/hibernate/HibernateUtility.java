8779f046e9c1556c7f7c28a23db12a998ce0a9e0
minassbond <minassbond@gmail.com>
2021-03-30 23:31:17 -0400

    Add files via upload
    Updated final project with DAO class for database connection

---
+++ PokedexDB/src/main/java/PokedexDB/hibernate/HibernateUtility.java
@@ -0,0 +1,35 @@
+package PokedexDB.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtility {
+
+    private static final SessionFactory session = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            ServiceRegistry servReg = new StandardServiceRegistryBuilder()//
+                    .configure("hibernate.cfg.xml").build();
+
+            Metadata metadata = new MetadataSources(servReg).getMetadataBuilder().build();
+
+            return metadata.getSessionFactoryBuilder().build();
+        } catch (Throwable ex) {
+            System.err.println("SessionFactory creation failed " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return session;
+    }
+
+    public static void shutdown() {
+
+        getSessionFactory().close();
+    }
+}
---
+++ PokedexDB/src/main/java/PokedexDB/hibernate/dao/PokedexDao.java
@@ -0,0 +1,68 @@
+package PokedexDB.hibernate.dao;
+
+import PokedexDB.hibernate.HibernateUtility;
+import PokedexDB.hibernate.model.Pokemon;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public class PokedexDao {
+
+    SessionFactory factory = null;
+    Session ses1 = null;
+
+    private static PokedexDao single_instance=null;
+
+    public PokedexDao(){
+        factory = HibernateUtility.getSessionFactory();
+    }
+
+    public static PokedexDao getInstance(){
+        {
+            if (single_instance == null){
+                single_instance = new PokedexDao();
+            }
+            return single_instance;
+        }
+    }
+
+    public List<Pokemon> getPokemon() {
+
+        try{
+            ses1 = factory.openSession();
+            ses1.getTransaction().begin();
+            String sql = "from PokedexDB.hibernate.model.Pokemon";
+            List<Pokemon> pk = (List<Pokemon>)ses1.createQuery(sql).getResultList();
+            ses1.getTransaction().commit();
+            return pk;
+        }catch (Exception e){
+            e.printStackTrace();
+            //Session rollback
+            ses1.getTransaction().rollback();
+            return null;
+        } finally{
+            ses1.close();
+        }
+    }
+
+    public Pokemon getPokemon(int pokeID){
+
+        try{
+            ses1 = factory.openSession();
+            ses1.getTransaction().begin();
+            String sql = "from PokedexDB.hibernate.model.Pokemon where id=" + Integer.toString(pokeID);
+            Pokemon p = (Pokemon)ses1.createQuery(sql).getSingleResult();
+            ses1.getTransaction().commit();
+            return p;
+        }catch (Exception e){
+            e.printStackTrace();
+            //Rollback
+            ses1.getTransaction().rollback();
+            return null;
+        }finally{
+            ses1.close();
+        }
+    }
+
+}
